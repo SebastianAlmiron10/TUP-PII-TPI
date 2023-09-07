75bafdfafcb4aa70e1011e47d0bd3d0b8601973f
Sebastian Almiron <sebastianr_almiron@hotmail.com>
2023-09-06 23:25:34 -0300

    funcion gestionar prestamo lista

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-# Trabajo Práctico I - Programación II
-
-
+# Trabajo Práctico I - Programación
+from libro import *
 import os
+
+
 
 print("Bienvenido!")
 respuesta = ''
+
+
+def gestionar_prestamo(codigo_busqueda):
+    
+    encontrado = False
+    for codigo in lista_libros:
+        if codigo_busqueda == codigo['cod']:
+            encontrado = True
+            
+            titulo_x = codigo['titulo']
+            autor_x = codigo['autor']
+            cant_x = codigo['cant_ej_ad']
+            
+            if cant_x > 0:
+                print(f"Titulo: {titulo_x}, Autor: {autor_x}, cantides disponibles: {cant_x}")
+                confirmado = int(input('confirmar prestamo (1. si 2. no): '))
+                if confirmado == 1:
+                    codigo['cant_ej_ad'] -= 1
+                    codigo['cant_ej_pr'] += 1
+            else:
+                print('no hay unidades disponibles')      
+        if not encontrado:
+            print('ERROR') 
+        
 
 def menu():
     print("1 - Gestionar Prestamo")
@@ -20,7 +45,8 @@ while respuesta != "salir":
     os.system ("cls") #Limpiar pantalla
     if opt.isnumeric():
         if int(opt) == 1:
-            #completar
+            codigo_busqueda = input('ingresar codigo de libro: ')
+            gestionar_prestamo(codigo_busqueda)
             print()
         elif int(opt) == 2:
             #completar
--- libro.py
+++ libro.py
@@ -1,12 +1,17 @@
 # Crear un diccionario para cada libro
-libro1 = {'cod': 'CRBJsAkS', 'cant_ej_ad': 3, 'cant_ej_pr': 1, "titulo": "Cien años de soledad", "autor": "Gabriel García Márquez"}
+libro1 = {'cod': 'asd', 'cant_ej_ad': 3, 'cant_ej_pr': 1, "titulo": "Cien años de soledad", "autor": "Gabriel García Márquez"}
 libro2 = {'cod': 'QgfV4j3c', 'cant_ej_ad': 4, 'cant_ej_pr': 2, "titulo": "El principito", "autor": "Antoine de Saint-Exupéry"}
 libro3 = {'cod': 'adOd09UE', 'cant_ej_ad': 1, 'cant_ej_pr': 0, "titulo": "El código Da Vinci", "autor": "Dan Brown"}
 
 def nuevo_libro():
-    #completar
+    #completarCRBJsAkS
     return None
 
 def generar_codigo():
     #completar
     return None
+
+lista_libros = []
+lista_libros.append(libro1)
+lista_libros.append(libro2)
+lista_libros.append(libro3)
